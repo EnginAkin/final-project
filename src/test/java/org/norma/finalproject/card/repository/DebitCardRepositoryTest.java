1a2d6545335d07b3d50ddaa6501cea98099c26fd
Engin <enginakin6363@gmail.com>
2022-06-04 19:04:20 +0300

    update

--- src/test/java/org/norma/finalproject/card/repository/DebitCardRepositoryTest.java
+++ src/test/java/org/norma/finalproject/card/repository/DebitCardRepositoryTest.java
@@ -31,42 +31,8 @@ public class DebitCardRepositoryTest {
 
     @BeforeEach
     public void setup(){
+        customerRepository.deleteAll();
         underTest.deleteAll();
-    }
-
-    @Test
-    public void givenCustomerID_whenFindAllCustomerDebitCards_thenReturnsCardList(){
-        // given
-        Long customerID=1L;
-        Customer customer=new Customer();
-        customer.setId(customerID);
-        customerRepository.save(customer);
-
-        CheckingAccount checkingAccount=new CheckingAccount();
-        checkingAccount.setBankCode("000000");
-        checkingAccount.setBranchCode("000000");
-        checkingAccount.setAccountName("account");
-        checkingAccount.setIbanNo("000000");
-        checkingAccount.setBranchName("branch");
-        checkingAccount.setAccountNo("000000");
-        checkingAccount.setBlocked(false);
-        checkingAccount.setCustomer(customer);
-        checkingAccountRepository.save(checkingAccount);
-
-        DebitCard debitCard1 =new DebitCard();
-        debitCard1.setCardNumber("11111");
-        debitCard1.setPassword("123");
-        debitCard1.setCvv("123");
-        debitCard1.setCheckingAccount(checkingAccount);
-
-        underTest.saveAll(List.of(debitCard1));
-
-        // when
-        List<DebitCard> debitCardListResponse = underTest.findAllByCheckingAccount_Customer_Id(customerID);
-        // then
-
-        Assertions.assertThat(debitCardListResponse.size()).isEqualTo(1);
-
     }
 
 
