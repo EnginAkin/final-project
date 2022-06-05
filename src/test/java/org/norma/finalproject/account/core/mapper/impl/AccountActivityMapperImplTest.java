09b5f63f28413c3594e88588e15353d85354d52b
Engin <enginakin6363@gmail.com>
2022-06-05 22:16:22 +0300

    activity mapper implemeted

---
+++ src/test/java/org/norma/finalproject/account/core/mapper/impl/AccountActivityMapperImplTest.java
@@ -0,0 +1,38 @@
+package org.norma.finalproject.account.core.mapper.impl;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.norma.finalproject.account.core.model.response.AccountActivityResponse;
+import org.norma.finalproject.account.entity.base.AccountActivity;
+import org.norma.finalproject.common.entity.enums.ActionStatus;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class AccountActivityMapperImplTest {
+
+    private AccountActivityMapperImpl accountActivityMapper=new AccountActivityMapperImpl();
+
+    @Test
+    public void givenAccountActivity_whenToDto_thenReturnAccountActivitiyResponse(){
+        // given
+        AccountActivity accountActivity=new AccountActivity();
+        accountActivity.setCrossAccount("111");
+        accountActivity.setActionStatus(ActionStatus.INCOMING);
+        accountActivity.setAvailableBalance(BigDecimal.TEN);
+        accountActivity.setAmount(BigDecimal.TEN);
+        accountActivity.setDescription("1111");
+        accountActivity.setDate(new Date());
+        // when
+        AccountActivityResponse accountActivityResponse = accountActivityMapper.toDto(accountActivity);
+        // then
+        Assertions.assertThat(accountActivityResponse).isNotNull();
+        Assertions.assertThat(accountActivityResponse.getAmount()).isEqualTo(BigDecimal.TEN);
+
+    }
+
+}
---
+++ src/test/java/org/norma/finalproject/account/core/mapper/impl/CheckingAccountMapperImplTest.java
@@ -0,0 +1,31 @@
+package org.norma.finalproject.account.core.mapper.impl;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.norma.finalproject.account.core.model.request.CreateCheckingAccountRequest;
+import org.norma.finalproject.account.entity.CheckingAccount;
+import org.norma.finalproject.account.entity.enums.CurrencyType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckingAccountMapperImplTest {
+
+    private CheckingAccountMapperImpl checkingAccountMapper=new CheckingAccountMapperImpl();
+
+    @Test
+    public void givenCreateCheckingAccountRequest_whenToEntity_thenReturnEntity(){
+        // given
+        CreateCheckingAccountRequest request=new CreateCheckingAccountRequest();
+        request.setBranchCode("11");
+        request.setBankCode("11");
+        request.setBranchName("11");
+        request.setCurrencyType(CurrencyType.TRY);
+        // when
+        CheckingAccount checkingAccount = checkingAccountMapper.toEntity(request);
+        // then
+        Assertions.assertThat(checkingAccount).isNotNull();
+        Assertions.assertThat(checkingAccount.getBankCode()).isEqualTo(request.getBankCode());
+
+    }
+
+}
